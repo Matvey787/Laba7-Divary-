d99ab55048747cb3719e6f41334537c10c193118
Matvey787 <matveyklg@gmail.com>
2025-02-06 12:28:44 +0300

    ccc

--- global.h
+++ global.h
@@ -1,6 +1,14 @@
 #ifndef GLOBAL
 #define GLOBAL
 
+
+typedef struct element_table {
+    char element_name[10];
+    unsigned int atomic_number;
+    unsigned int html_cod;
+    float atomic_mass;
+} Elem_Table;
+
 typedef struct formula {
     int title;
     unsigned int amount;
@@ -11,5 +19,10 @@ typedef struct drug {
     Formula drug_formula [10];
     int additive[5];
 } Drug;
+
+typedef struct list{
+    Drug elem;
+    struct list *next;
+} List;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 int main() {
     Drug passport;
+    List *hash_table[71];
+    
 }
 
 void read_table (Elem_Table * main_table)
